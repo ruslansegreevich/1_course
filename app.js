e3f70c6a0ec53dc5cd1636d795f1a26c862c6c0a
ruslansegreevich <r.ogrenich@inbox.ru>
2023-11-15 21:44:52 +0300

    1 задача

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+const tax = 80;
+const timeWork = (11 - 2) * 5;
+const timeOrder = 40;
+
+console.log('Смогу ли я работать? ' + (timeWork >= timeOrder));
+console.log('Стоимость работ: ' + (tax * timeOrder));
