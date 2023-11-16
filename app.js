de3fd879ddb818bb68e7b2b4e29a1131b5a54061
ruslansegreevich <r.ogrenich@inbox.ru>
2023-11-16 18:37:25 +0300

    2 задача

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-console.log('Hello PAVEL')
+const deposit = 12000;
+const percent = 7 / 100;
+const house = 13500;
 
-//  17 урок !!!!
+const finalDeposit = deposit * (1 + (percent / 12)) ** 24;
+
+if (finalDeposit >= house) {
+    console.log(`Вы можете купить дом и у Вас останется: ${finalDeposit - house} $`);
+}
+else {
+    console.log(`Вы не можете купить дом, у Вас : ${finalDeposit} $`);
+}
