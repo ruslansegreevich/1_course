df949f304ae2e7684fb661575edfdb0d2b56bfce
ruslansegreevich <r.ogrenich@inbox.ru>
2023-11-18 21:50:25 +0300

    5 задача

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-function checkGetBook(age, job, money) {
-        if ((money >= 2000) || (age > 24 && job === true && money >= 1500) || (age > 24 && job === false && money >= 1900)) {
-            return true
-        } else {
-            return false;
-        }
-
-}
-
-console.log(checkGetBook(12, false, 0));
+// 44 Урокк
+// Раздел 8 Массивы
