2a643121b099c4f8f1ff1c9ae6784897df3f0cdf
ruslansegreevich <r.ogrenich@inbox.ru>
2023-11-23 12:40:55 +0300

    48 упражнение - Функция разбора URL

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-// 44 Урокк
-// Раздел 8 Массивы
-function add(task) {
+/*
+    Дан прпоизвольный url -
+    'https://purpleschool.ru/course/javascript'
+    Нужно сделать функцию, которая выводит в консоль:
+    - Протокол (https)
+    - Доменное имя (purpleschool.ru)
+    - Путь внутри сайта (/course/javascript)
+ */
 
+const url = 'https://purpleschool.ru/course/javascript';
+
+function result(url) {
+    const resurl = url.split('/');
+    const [protocol, _, domen, saite, saite1] = resurl;
+    if (protocol === 'https:' || protocol === 'http:') {
+console.log(`Протокол: ${protocol.slice(0, -1)} 
+Доменное имя: ${domen}
+Путь внутри сайта: /${saite}/${saite1}`);
+    } else {
+        return console.log('Не верно указан URL !!!');
+    }
 }
+result(url);
