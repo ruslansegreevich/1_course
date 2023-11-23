cb0faf5a13aa213acfd802dd9f2790ab58b4c6d7
ruslansegreevich <r.ogrenich@inbox.ru>
2023-11-23 17:24:00 +0300

    цикл

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 /*
-    Дан прпоизвольный url -
-    'https://purpleschool.ru/course/javascript'
-    Нужно сделать функцию, которая выводит в консоль:
-    - Протокол (https)
-    - Доменное имя (purpleschool.ru)
-    - Путь внутри сайта (/course/javascript)
+    Задача вывести в консоль строку "Я люблю JS !" из массива,
+    проходя циклом в обратном порядкуе, не используя метод revers.
+    const arr = ['!', 'JS', 'люблю', 'Я'];
  */
 
-const url = 'https://purpleschool.ru/course/javascript';
+const arr = ['!', 'JS', 'люблю', 'Я'];
 
-function result(url) {
-    const resurl = url.split('/');
-    const [protocol, _, domen, saite, saite1] = resurl;
-    if (protocol === 'https:' || protocol === 'http:') {
-console.log(`Протокол: ${protocol.slice(0, -1)} 
-Доменное имя: ${domen}
-Путь внутри сайта: /${saite}/${saite1}`);
-    } else {
-        return console.log('Не верно указан URL !!!');
-    }
+let newArray = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i]);
+
 }
-result(url);
+console.log(newArray.join(' '));
+console.log(arr.reverse().join(' '));
