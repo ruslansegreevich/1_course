92f67b4dc9afc493b33a74c0b55e627fe14b90a4
ruslansegreevich <r.ogrenich@inbox.ru>
2023-11-29 21:26:36 +0300

    78 Упражнение

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// 68 УРОК
+const card = '2342834503458353';
+
+function secret(card) {
+    if (card.length !== 16) {
+        return console.log('Неверно введена карта')
+    } else if (isNaN(Number(card)) === true) {
+        return console.log('Неверно введена карта')
+    } else {
+        card.slice(0 , 12);
+        return console.log(card.slice(12).padStart(12, '*'));
+    }
+}
+
+
+
+secret(card);
